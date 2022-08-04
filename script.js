64645fe9c50ad008d38d686b60c9354e867bd55e
Tahsin_Toaha <toahachy6363@gmail.com>
2022-08-04 22:07:33 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function getHistory() {
+    return document.getElementById("history-value").innerText;
+}
+ function printHistory(num) {
+     document.getElementById("history-value").innerText=num;
+ }
+ function getOutput() {
+     return document.getElementById("output-value").innerText;
+ }
+ function printOutput(num) {
+     if (num == "" ){
+         document.getElementById("output-value").innerText=num;
+     } else{
+      document.getElementById("output-value").innerText= getFormattedNumber(num);
+ }
+ function getFormattedNumber(num) {
+     var n = Number(num);
+     var value = n.toLocaleString("en");
+     return value;
+ }
+ printOutput("76678677")
+ function reverseNumberFormat(num) {
+     return Number(num.replace(/,/g,''));
+ }
+ alert(reverseNumberFormat(getOutput()))
